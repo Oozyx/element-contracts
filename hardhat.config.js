50f0cbbba6760e61603d7e853434988121531302
Oozyx <hendrik.oosenbrug@gmail.com>
2022-07-31 10:56:00 -0400

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
